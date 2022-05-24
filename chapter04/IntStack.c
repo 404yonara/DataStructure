dff5ade6438528c32767650ae0d0ab5686277fdc
Hogwon Kim <hogkim@c4r10s7.42seoul.kr>
2022-05-24 11:30:34 +0900

    1

---
+++ chapter04/IntStack.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "IntStack.h"
+
+int	Initialize(IntStack *s, int max)
+{
+	IntStack	*newStack;
+	newStack = (IntStack *)malloc(IntStack);
+	newStack->stk
+}
